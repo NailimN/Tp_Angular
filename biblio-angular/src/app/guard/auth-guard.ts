d6cb047f9f8c6a66837dc4a5c6c8eded8ba7c8c9
Martin_G <mgabut.pro@gmail.com>
2025-11-14 15:10:15 +0100

    interceptors

--- biblio-angular/src/app/guard/auth-guard.ts
+++ biblio-angular/src/app/guard/auth-guard.ts
@@ -2,4 +2,15 @@ import { CanActivateFn } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
   return true;
+
+  // const router = inject(Router);
+  // const authService = inject(AuthService);
+
+  // if (authService.token) {
+  //   // Ici, si on a un token, c'est qu'on est connecté
+  //   return true;
+  // }
+
+  // // Navigation à utiliser dans les guard
+  // return router.createUrlTree([ '/login' ]);
 };
