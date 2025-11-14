85861adc0df99b6142eae660d2c7b710ae6e47be
mplichon <plichon.matteo@gmail.com>
2025-11-14 14:57:46 +0100

    ajout de l'api rest de Editeur

--- biblio-boot/src/main/java/biblio_boot/rest/EditeurRestController.java
+++ biblio-boot/src/main/java/biblio_boot/rest/EditeurRestController.java
@@ -1,10 +1,58 @@
 package biblio_boot.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import biblio_boot.model.Editeur;
+import biblio_boot.service.EditeurService;
 
 @RestController
 @RequestMapping("/api/editeur")
 public class EditeurRestController {
+	
+	@Autowired
+	EditeurService editeurSrv;
 
+
+	@GetMapping
+	public List<Editeur> allEditeurs()
+	{
+		return editeurSrv.getAll();
+	}
+
+
+	@GetMapping("/{id}")
+	public Editeur ficheEditeur(@PathVariable Integer id, Editeur editeur) {
+		return editeurSrv.getById(id);
+	}
+
+
+	@PostMapping
+	public Editeur ajoutEditeur(@RequestBody Editeur editeur)
+	{
+		return editeurSrv.create(editeur);
+	}
+
+
+	@PutMapping("/{id}")
+	public Editeur modifierEditeur(@PathVariable Integer id,@RequestBody Editeur editeur)
+	{
+		editeur.setId(id);
+		return (Editeur) editeurSrv.update(editeur);
+	}
+
+
+	@DeleteMapping("/{id}")
+	public void supprimerEditeur(@PathVariable Integer id) {
+		editeurSrv.deleteById(id);
+	}
 }
