21609a6fa79bcb0dd72a7547e8a31247a5ec4d62
Alyssa <tasafut013@gmail.com>
2025-11-14 18:04:14 +0100

    Collection Serv + page
    Ajout des services et des pages

--- biblio-angular/src/app/page/collection/collection-page/collection-page.ts
+++ biblio-angular/src/app/page/collection/collection-page/collection-page.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CollectionDto } from '../../../dto/collection-dto';
+import { CollectionService } from '../../../service/collection-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collection-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+    FormsModule
+  ],
   templateUrl: './collection-page.html',
   styleUrl: './collection-page.css',
 })
-export class CollectionPage {
+export class CollectionPage implements OnInit {
+  protected collection: CollectionDto = new CollectionDto(0, "")
+  protected collections$!: Observable<CollectionDto[]>;
 
+  constructor(private collectionService: CollectionService) {}
+
+  ngOnInit(): void {
+    this.collections$ = this.collectionService.findAll();
+  }
+
+    public ajouterCollection() {
+      this.collectionService.save(this.collection);
+  
+      this.collection = new CollectionDto(0, "");
+    }
+  
+    public modifierCollection() {
+      this.collectionService.save(this.collection);
+  
+      this.collection = new CollectionDto(0, "");
+    }
+  
+    public editCollection(collection: CollectionDto): void {
+      // Clone du TODO pour l'édition
+      this.collection = new CollectionDto(collection.id, collection.nom);
+    }
+  
+    public deleteCollection(collection: CollectionDto): void {
+      this.collectionService.deleteById(collection.id);
+    }
+  
+
+
+  public trackCollection(index: number, value: CollectionDto) {
+    return value.id; // Getter utilisé
+  }
 }
--- biblio-angular/src/app/service/collection-service.ts
+++ biblio-angular/src/app/service/collection-service.ts
@@ -1,8 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, startWith, Subject, switchMap } from 'rxjs';
+import { CollectionDto } from '../../../dto/collection-dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CollectionService {
-  
+  private apiUrl: string = '/collection';
+  private refresh$: Subject<void> = new Subject<void>();
+
+  constructor(private http: HttpClient) { }
+
+  public findAll(): Observable<CollectionDto[]> {
+    return this.refresh$.pipe( // permet de transformer un flux / manipuler un flux
+      // Forcer un premier chargement
+      startWith(null),
+
+      // Transformer le "void" que MatiereDto[] en allant chercher les informations
+      switchMap(() => {
+        return this.http.get<CollectionDto[]>(this.apiUrl);
+      })
+    );
+  }
+
+  public refresh() {
+    this.refresh$.next(); // Permet d'envoyer des nouvelles infos
+  }
+
+  public findById(id: number): Observable<CollectionDto> {
+    return this.http.get<CollectionDto>(`${ this.apiUrl }/${ id }`);
+  }
+
+  public save(collectionDto: CollectionDto): void {
+    const payload = collectionDto.toJson();
+
+    if (!collectionDto.id) {
+      this.http.post<CollectionDto>(this.apiUrl, payload).subscribe(() => this.refresh());
+    }
+
+    this.http.put<CollectionDto>(`${ this.apiUrl }/${ collectionDto.id }`, payload).subscribe(() => this.refresh());
+  }
+
+  public deleteById(id: number): void {
+    this.http.delete<void>(`${ this.apiUrl }/${ id }`).subscribe(() => this.refresh());
+  }
 }
