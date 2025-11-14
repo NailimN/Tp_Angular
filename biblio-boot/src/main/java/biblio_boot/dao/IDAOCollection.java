64a80e10cf1655699128f5f7086cbf1ae04a4ad6
Alyssa <tasafut013@gmail.com>
2025-11-14 12:56:43 +0100

    Collection
    IDAO + model + rest+ service

---
+++ biblio-boot/src/main/java/biblio_boot/dao/IDAOCollection.java
@@ -0,0 +1,8 @@
+package biblio_boot.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import biblio_boot.model.Collection;
+
+public interface IDAOCollection extends JpaRepository<Collection, Long> {
+
+}
