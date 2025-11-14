4eb448dee02635bb0dda8a78fb43e4e67d793c2f
NailimN <milian.normantlegall@gmail.com>
2025-11-14 12:19:47 +0100

    Initialisation projet Spring + Angular

---
+++ biblio-boot/src/main/java/biblio_boot/BiblioBootApplication.java
@@ -0,0 +1,13 @@
+package biblio_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioBootApplication.class, args);
+	}
+
+}
