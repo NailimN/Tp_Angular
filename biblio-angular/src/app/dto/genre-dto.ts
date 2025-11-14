5330c252453ab4afe44a1c4fccdfe3749f1e56d0
Martin_G <mgabut.pro@gmail.com>
2025-11-14 17:06:17 +0100

    genre angular

--- biblio-angular/src/app/dto/genre-dto.ts
+++ biblio-angular/src/app/dto/genre-dto.ts
@@ -1,2 +1,31 @@
 export class GenreDto {
+    constructor(
+        public _id: number,
+        public _libelle: string
+    ) { }
+
+    public get id(): number {
+        return this._id;
+    }
+    public get libelle(): string {
+        return this._libelle;
+    }
+    public set libelle(value: string) {
+        this._libelle = value;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+    public toJsonWithId(): any {
+        return {
+            id: this._id,
+            libelle: this._libelle
+        };
+    }
+
+    public toJson(): any {
+        return {
+            libelle: this._libelle
+        };
+    }
 }
--- biblio-angular/src/app/page/genre/genre-page/genre-page.ts
+++ biblio-angular/src/app/page/genre/genre-page/genre-page.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GenreDto } from '../../../dto/genre-dto';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { GenreService } from '../../../service/genre-service';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-genre-page',
-  imports: [],
+  imports: [ CommonModule, RouterLink, ReactiveFormsModule ],
   templateUrl: './genre-page.html',
   styleUrl: './genre-page.css',
 })
-export class GenrePage {
+export class GenrePage implements OnInit {
+
+  protected genres$!: Observable<GenreDto[]>;
+  protected genreForm!: FormGroup;
+  protected libelleCtrl!: FormControl;
+  protected editingGenre!: GenreDto | null;
+
+  constructor(private genreService: GenreService, private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.genres$ = this.genreService.findAll();
+
+    this.libelleCtrl = new FormControl('', Validators.required);
+
+    this.genreForm = this.formBuilder.group({
+      libelle: this.libelleCtrl
+    });
+  }
+
+  public trackGenre(index: number, genre: GenreDto): number {
+    return genre.id;
+  }
+
+  public creerOuModifier() {
+    if (this.editingGenre) {
+      this.genreService.save(new GenreDto(this.editingGenre.id, this.libelleCtrl.value));
+    }
+  
+    else {
+      this.genreService.save(new GenreDto(0, this.libelleCtrl.value));
+    }
+
+    this.editingGenre = null;
+    this.libelleCtrl.reset();
+  }
+
+  public editer(genre: GenreDto) {
+    this.editingGenre = genre;
+    this.libelleCtrl.setValue(genre.libelle);
+  }
+
+  public supprimer(genre: GenreDto) {
+    this.genreService.deleteById(genre.id);
+  }
 
 }
