b048996df67120aebc0c7058ad76635b5b2ef2bd
salom <salome.allali.kg@gmail.com>
2025-11-14 16:14:50 +0100

    auteur page

--- biblio-angular/src/app/page/auteur/auteur-page/auteur-page.ts
+++ biblio-angular/src/app/page/auteur/auteur-page/auteur-page.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuteurDto } from '../../../dto/auteur-dto';
+import { AuteurService } from '../../../service/auteur-service';
 
 @Component({
   selector: 'app-auteur-page',
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './auteur-page.html',
   styleUrl: './auteur-page.css',
 })
-export class AuteurPage {
+export class AuteurPage implements OnInit {
+protected auteurs$!: Observable<AuteurDto[]>;
+
+constructor(private auteurService: AuteurService) { }
+
+  ngOnInit(): void {
+    this.auteurs$ = this.auteurService.findAll();
+  }
+
+  public trackAuteur(index: number, value: AuteurDto) {
+    return value.id;
+  }
 
 }
