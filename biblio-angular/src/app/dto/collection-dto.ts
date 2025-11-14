0a529e273e3371057752284f3deb6c9d45cff5d5
Alyssa <tasafut013@gmail.com>
2025-11-14 17:11:23 +0100

    collec
    dto

--- biblio-angular/src/app/dto/collection-dto.ts
+++ biblio-angular/src/app/dto/collection-dto.ts
@@ -1,2 +1,34 @@
 export class CollectionDto {
+      constructor(private _id: number, private _nom: string) { }
+
+    public get id(): number {
+        return this._id;
+    }
+
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    public get nom(): string {
+        return this._nom;
+    }
+
+    public set nom(value: string) {
+        this._nom = value;
+    }
+
+    public toJson(): any {
+        return {
+            nom: this.nom,
+        };
+    }
+
+    public toJsonWithId(): any {
+        return {
+            id: this.id,
+            nom: this.nom,
+        };
+    }
+}
+
 }
