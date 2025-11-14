a172900b78ac142d92b02eca4ea71e2e7d913a4c
mplichon <plichon.matteo@gmail.com>
2025-11-14 12:49:42 +0100

    mapping Editeur

--- biblio-boot/src/main/java/biblio_boot/model/Editeur.java
+++ biblio-boot/src/main/java/biblio_boot/model/Editeur.java
@@ -1,14 +1,38 @@
 package biblio_boot.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="editeur")
 public class Editeur {
 	
 	// Attributs
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
 	private String nom;
+	
+	@Column(nullable = false)
 	private String pays;
 	
 	
 	// Constructeurs
+	public Editeur() {}
+	
 	public Editeur(String nom, String pays) {
+		this.nom = nom;
+		this.pays = pays;
+	}
+	
+	public Editeur(Integer id, String nom, String pays) {
+		this.id = id;
 		this.nom = nom;
 		this.pays = pays;
 	}
@@ -19,22 +43,19 @@ public class Editeur {
 		return nom;
 	}
 
-
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
-
 
 	public String getPays() {
 		return pays;
 	}
 
-
 	public void setPays(String pays) {
 		this.pays = pays;
 	}
 
-
+	
 	// toString
 	@Override
 	public String toString() {
