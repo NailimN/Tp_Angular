aa0a502d301e13bfe8ae0cf493c7106b8c987aa4
mplichon <plichon.matteo@gmail.com>
2025-11-14 16:18:26 +0100

    ajout de Editeur (listes des éditeurs) dans angular + ajout des cross origin

--- biblio-angular/src/app/page/editeur/editeur-page/editeur-page.ts
+++ biblio-angular/src/app/page/editeur/editeur-page/editeur-page.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Observable } from 'rxjs';
+import { EditeurDto } from '../../../dto/editeur-dto';
+import { EditeurService } from '../../../service/editeur-service';
 
 @Component({
   selector: 'app-editeur-page',
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './editeur-page.html',
   styleUrl: './editeur-page.css',
 })
-export class EditeurPage {
+export class EditeurPage implements OnInit{
+  protected editeurs$!: Observable<EditeurDto[]>;
 
+  constructor(private editeurService: EditeurService) { }
+
+  ngOnInit(): void {
+    this.editeurs$ = this.editeurService.findAll();
+  }
+
+  public trackEditeur(index: number, value: EditeurDto) {
+    return value.id;
+  }
 }
