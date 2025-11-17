8911eb2c43ee0907153712a28bd168009eb9cce5
mplichon <plichon.matteo@gmail.com>
2025-11-17 13:16:55 +0100

    ajout d'un formulaire réactif pour éditeur

--- biblio-angular/src/app/page/editeur/editeur-page/editeur-page.ts
+++ biblio-angular/src/app/page/editeur/editeur-page/editeur-page.ts
@@ -4,43 +4,61 @@ import { RouterLink } from '@angular/router';
 import { Observable } from 'rxjs';
 import { EditeurDto } from '../../../dto/editeur-dto';
 import { EditeurService } from '../../../service/editeur-service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editeur-page',
   imports: [
     CommonModule,
     RouterLink,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './editeur-page.html',
   styleUrl: './editeur-page.css',
 })
-export class EditeurPage implements OnInit{
+export class EditeurPage implements OnInit {
   protected editeur: EditeurDto = new EditeurDto(0, "", "");
   protected editeurs$!: Observable<EditeurDto[]>;
+  protected editeurForm!: FormGroup;
+  protected nomCtrl!: FormControl;
+  protected paysCtrl!: FormControl;
+  protected editingEditeur!: EditeurDto | null;
 
-  constructor(private editeurService: EditeurService) { }
+  constructor(private editeurService: EditeurService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.editeurs$ = this.editeurService.findAll();
+
+    this.nomCtrl = new FormControl('', Validators.required);
+    this.paysCtrl = new FormControl('', Validators.required);
+
+    this.editeurForm = this.formBuilder.group({
+      nom: this.nomCtrl,
+      pays: this.paysCtrl
+    });
   }
 
-  public ajouterEditeur() {
-    this.editeurService.save(this.editeur);
+  public ajouterModifierEditeur() {
 
-    this.editeur = new EditeurDto(0, "", "");
-  }
+    if (this.editingEditeur) {
+      this.editeurService.save(new EditeurDto(this.editingEditeur.id, this.nomCtrl.value, this.paysCtrl.value));
+    }
 
-  public modifierEditeur() {
-    this.editeurService.save(this.editeur);
+    else {
+      this.editeurService.save(new EditeurDto(0, this.nomCtrl.value, this.paysCtrl.value));
+    }
 
-    this.editeur = new EditeurDto(0, "", "");
+    this.editingEditeur = null;
+    this.nomCtrl.reset();
+    this.paysCtrl.reset();
   }
 
   public editEditeur(editeur: EditeurDto): void {
     // Clone du TODO pour l'édition
-    this.editeur = new EditeurDto(editeur.id, editeur.nom, editeur.pays);
+    this.editingEditeur = editeur;
+    this.nomCtrl.setValue(editeur.nom);
+    this.paysCtrl.setValue(editeur.pays);
   }
 
   public deleteEditeur(editeur: EditeurDto): void {
