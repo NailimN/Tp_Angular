17a2752d4ec84ce34a5464bdf7149f364fa92a19
salom <salome.allali.kg@gmail.com>
2025-11-17 12:30:30 +0100

    required et css

--- biblio-angular/src/app/page/auteur/auteur-page/auteur-page.ts
+++ biblio-angular/src/app/page/auteur/auteur-page/auteur-page.ts
@@ -4,39 +4,69 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuteurDto } from '../../../dto/auteur-dto';
 import { AuteurService } from '../../../service/auteur-service';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auteur-page',
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './auteur-page.html',
   styleUrl: './auteur-page.css',
 })
 export class AuteurPage implements OnInit {
+
 protected auteur : AuteurDto = new AuteurDto(0,"","","") 
 protected auteurs$!: Observable<AuteurDto[]>;
+protected auteurForm! : FormGroup;
+protected nameCtrl!: FormControl;
+protected preCtrl!:FormControl ;
+protected natCtrl!: FormControl ;
+protected editingAuteur!: AuteurDto | null;
 
-constructor(private auteurService: AuteurService) { }
+
+constructor(private auteurService: AuteurService,  private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.auteurs$ = this.auteurService.findAll();
+
+    this.nameCtrl = new FormControl('', Validators.required);
+this.preCtrl = new FormControl();
+this.natCtrl = new FormControl();
+
+    this.auteurForm = this.formBuilder.group({
+     nom : this.nameCtrl,
+     prenom : this.preCtrl,
+     nationalite : this.natCtrl
+    });
+
   }
 
-    public ajouterAuteur() {
-      this.auteurService.save(this.auteur);
-  
-      this.auteur = new AuteurDto(0, "", "", "");
+    public ajoutermodifierAuteur() {
+      
+       if (this.editingAuteur) {
+            this.auteurService.save(new AuteurDto(this.editingAuteur.id, this.nameCtrl.value,this.preCtrl.value,this.natCtrl.value ));
+          }
+        
+          else {
+            this.auteurService.save(new AuteurDto(0, this.nameCtrl.value,this.preCtrl.value ?? "",this.natCtrl.value ?? ""));
+
+          }
+      
+          this.editingAuteur = null;
+          this.nameCtrl.reset();
+           this.preCtrl.reset();
+            this.natCtrl.reset();
+
+        
     }
   
-    public modifierAuteur() {
-      this.auteurService.save(this.auteur);
   
-      this.auteur = new AuteurDto(0, "", "", "");
-    }
   
     public editAuteur(auteur: AuteurDto): void {
       // Clone du TODO pour l'édition
-      this.auteur = new AuteurDto(auteur.id, auteur.nom, auteur.prenom, auteur.nationalite);
+      this.editingAuteur = auteur;
+     this.nameCtrl.setValue(auteur.nom);
+     this.preCtrl.setValue(auteur.prenom);
+     this.natCtrl.setValue(auteur.nationalite);
     }
   
     public deleteAuteur(auteur: AuteurDto): void {
