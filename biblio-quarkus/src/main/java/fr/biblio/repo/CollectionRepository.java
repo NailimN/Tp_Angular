620ace6ce2cb3db0b074da925c01e8e418c6b942
NailimN <milian.normantlegall@gmail.com>
2025-11-28 10:35:59 +0100

    model collection

--- biblio-quarkus/src/main/java/fr/biblio/repo/CollectionRepository.java
+++ biblio-quarkus/src/main/java/fr/biblio/repo/CollectionRepository.java
@@ -1,6 +1,6 @@
 package fr.biblio.repo;
 
-import java.util.Collection;
+import fr.biblio.model.Collection;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
